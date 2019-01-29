6a17a0189140ba1f258d9f031e9d01ffcf9f940f
Messiah <Taylor.Jonathan@aol.com>
2019-01-28 21:16:32 -0500

    mainJs

---
+++ Assets/js/main.js
@@ -0,0 +1,77 @@
+$(document).ready(function () {
+
+    
+        var numberOne = 0;
+        var numberTwo = 0;
+        var operator = "";
+        var result = 0;
+        var operatorChosen = false;
+        var calculated = false;
+
+    
+        function reset() {
+            numberOne = " ";
+            numberTwo = " ";
+            operator = "";
+            operatorChosen = false;
+            calculated = false;
+
+            $('#result,#operator,#numberOne,#numberTwo').empty();
+        }
+
+        $('.number').on("click", function(){
+            if(calculated) {
+                return false;
+            }
+            if(operatorChosen){
+                numberTwo += $(this).val();
+                console.log(numberTwo)
+                $('.numberTwo').text(numberTwo);
+            }else {
+                numberOne += $(this).val();
+                $('.numberOne').text(numberOne);
+            }
+
+        })
+        $('.operator').on('click', function(){
+        if(!numberOne || calculated){
+            return false;
+        }
+
+        operatorChosen = true;
+        operator = $(this).val();
+        console.log(operator);
+        $('#operator').text($(this).text());
+    })
+       $('.equal').on('click', function(){
+        if ( calculated){
+            return false;
+        }
+            calculated = true;
+
+            numberOne = parseInt(numberOne);
+            numberTwo = parseInt(numberTwo);
+
+            if (operator === "plus"){
+                result = numberOne + numberTwo;
+            }
+            else if (operator === 'minus'){
+                result = numberOne - numberTwo;
+            }
+            else if (operator === 'times'){
+                result = numberOne * numberTwo;
+            }
+            else if(operator === ' divide'){
+                result = numberOne / numberTwo;
+            }
+            else if(operator === 'power'){
+                result = Math.pow(numberOne, numberTwo);
+            }
+            $('#result').text(result);
+
+       })
+       $(".clear").on('click', function(){
+           reset();
+       })
+       reset();
+    });
